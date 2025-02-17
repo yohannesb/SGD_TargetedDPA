791ae4da5ee25ca11ab259fdc04c77b92875624f
yohannesb <yohannesb@users.noreply.github.com>
2025-02-17 17:09:47 +0300

    Update client.py

--- client.py
+++ client.py
@@ -28,7 +28,7 @@ class Client:
 
         self.loss_function = self.args.get_loss_function()()
         
-        Use Adam optimizer
+        #Use Adam optimizer
         self.optimizer = optim.Adam(self.net.parameters(),
             lr=self.args.get_learning_rate(),  # Learning rate
             betas=(self.args.get_beta1(), self.args.get_beta2()),  # Adam's beta parameters
