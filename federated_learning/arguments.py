1a9fdd3d7e93702895f71ca73178e741d7dacdc7
yohannesb <yohannesb@users.noreply.github.com>
2025-02-22 14:44:17 +0300

    Update arguments.py

--- federated_learning/arguments.py
+++ federated_learning/arguments.py
@@ -46,8 +46,8 @@ class Arguments:
         self.epoch_save_start_suffix = "start"
         self.epoch_save_end_suffix = "end"
 
-        self.num_workers = 1
-        self.num_poisoned_workers = 1
+        self.num_workers = 50
+        self.num_poisoned_workers = 50
 
         # self.num_workers = 1
         # self.num_poisoned_workers = 0
