620ca26a9e8caf8fe221b01d5745ddf52dfa0d97
yohannesb <yohannesb@users.noreply.github.com>
2025-02-24 18:42:04 +0300

    Update generate_data_distribution.py

--- generate_data_distribution.py
+++ generate_data_distribution.py
@@ -34,18 +34,18 @@ if __name__ == '__main__':
     # ---------------------------------
     # --------- Fashion-MNIST ---------
     # ---------------------------------
-    dataset = FashionMNISTDataset(args)
-    TRAIN_DATA_LOADER_FILE_PATH = "data_loaders/fashion-mnist/train_data_loader.pickle"
-    TEST_DATA_LOADER_FILE_PATH = "data_loaders/fashion-mnist/test_data_loader.pickle"
+    # dataset = FashionMNISTDataset(args)
+    # TRAIN_DATA_LOADER_FILE_PATH = "data_loaders/fashion-mnist/train_data_loader.pickle"
+    # TEST_DATA_LOADER_FILE_PATH = "data_loaders/fashion-mnist/test_data_loader.pickle"
 
-    if not os.path.exists("data_loaders/fashion-mnist"):
-        pathlib.Path("data_loaders/fashion-mnist").mkdir(parents=True, exist_ok=True)
+    # if not os.path.exists("data_loaders/fashion-mnist"):
+    #     pathlib.Path("data_loaders/fashion-mnist").mkdir(parents=True, exist_ok=True)
 
-    train_data_loader = generate_train_loader(args, dataset)
-    test_data_loader = generate_test_loader(args, dataset)
+    # train_data_loader = generate_train_loader(args, dataset)
+    # test_data_loader = generate_test_loader(args, dataset)
 
-    with open(TRAIN_DATA_LOADER_FILE_PATH, "wb") as f:
-        save_data_loader_to_file(train_data_loader, f)
+    # with open(TRAIN_DATA_LOADER_FILE_PATH, "wb") as f:
+    #     save_data_loader_to_file(train_data_loader, f)
 
-    with open(TEST_DATA_LOADER_FILE_PATH, "wb") as f:
-        save_data_loader_to_file(test_data_loader, f)
+    # with open(TEST_DATA_LOADER_FILE_PATH, "wb") as f:
+    #     save_data_loader_to_file(test_data_loader, f)
