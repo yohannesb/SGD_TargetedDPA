6b0865edd41fef2fb5165044fc1a63b704e95e78
yohannesb <yohannesb@users.noreply.github.com>
2025-02-24 18:43:06 +0300

    Update arguments.py

--- federated_learning/arguments.py
+++ federated_learning/arguments.py
@@ -33,9 +33,9 @@ class Arguments:
         self.eps = 1e-8
 
         #Adam
-        self.beta1 = 0.9
-        self.beta2 = 0.999
-        self.eps = 1e-8
+        # self.beta1 = 0.9
+        # self.beta2 = 0.999
+        # self.eps = 1e-8
 
         self.round_worker_selection_strategy = None
         self.round_worker_selection_strategy_kwargs = None
