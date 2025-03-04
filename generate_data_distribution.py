575d297b1cae7ad8791ff8910eac83ebb2e9cf8d
yohannesb <yohannesb@users.noreply.github.com>
2025-03-04 17:58:38 +0300

    Update generate_data_distribution.py

--- generate_data_distribution.py
+++ generate_data_distribution.py
@@ -15,31 +15,12 @@ if __name__ == '__main__':
     # ---------------------------------
     # ------------ CIFAR10 ------------
     # ---------------------------------
-    dataset = CIFAR10Dataset(args)
-    TRAIN_DATA_LOADER_FILE_PATH = "data_loaders/cifar10/train_data_loader.pickle"
-    TEST_DATA_LOADER_FILE_PATH = "data_loaders/cifar10/test_data_loader.pickle"
+    # dataset = CIFAR10Dataset(args)
+    # TRAIN_DATA_LOADER_FILE_PATH = "data_loaders/cifar10/train_data_loader.pickle"
+    # TEST_DATA_LOADER_FILE_PATH = "data_loaders/cifar10/test_data_loader.pickle"
 
-    if not os.path.exists("data_loaders/cifar10"):
-        pathlib.Path("data_loaders/cifar10").mkdir(parents=True, exist_ok=True)
-
-    train_data_loader = generate_train_loader(args, dataset)
-    test_data_loader = generate_test_loader(args, dataset)
-
-    with open(TRAIN_DATA_LOADER_FILE_PATH, "wb") as f:
-        save_data_loader_to_file(train_data_loader, f)
-
-    with open(TEST_DATA_LOADER_FILE_PATH, "wb") as f:
-        save_data_loader_to_file(test_data_loader, f)
-
-    # ---------------------------------
-    # --------- Fashion-MNIST ---------
-    # ---------------------------------
-    # dataset = FashionMNISTDataset(args)
-    # TRAIN_DATA_LOADER_FILE_PATH = "data_loaders/fashion-mnist/train_data_loader.pickle"
-    # TEST_DATA_LOADER_FILE_PATH = "data_loaders/fashion-mnist/test_data_loader.pickle"
-
-    # if not os.path.exists("data_loaders/fashion-mnist"):
-    #     pathlib.Path("data_loaders/fashion-mnist").mkdir(parents=True, exist_ok=True)
+    # if not os.path.exists("data_loaders/cifar10"):
+    #     pathlib.Path("data_loaders/cifar10").mkdir(parents=True, exist_ok=True)
 
     # train_data_loader = generate_train_loader(args, dataset)
     # test_data_loader = generate_test_loader(args, dataset)
@@ -49,3 +30,22 @@ if __name__ == '__main__':
 
     # with open(TEST_DATA_LOADER_FILE_PATH, "wb") as f:
     #     save_data_loader_to_file(test_data_loader, f)
+
+    # ---------------------------------
+    # --------- Fashion-MNIST ---------
+    # ---------------------------------
+    dataset = FashionMNISTDataset(args)
+    TRAIN_DATA_LOADER_FILE_PATH = "data_loaders/fashion-mnist/train_data_loader.pickle"
+    TEST_DATA_LOADER_FILE_PATH = "data_loaders/fashion-mnist/test_data_loader.pickle"
+
+    if not os.path.exists("data_loaders/fashion-mnist"):
+        pathlib.Path("data_loaders/fashion-mnist").mkdir(parents=True, exist_ok=True)
+
+    train_data_loader = generate_train_loader(args, dataset)
+    test_data_loader = generate_test_loader(args, dataset)
+
+    with open(TRAIN_DATA_LOADER_FILE_PATH, "wb") as f:
+        save_data_loader_to_file(train_data_loader, f)
+
+    with open(TEST_DATA_LOADER_FILE_PATH, "wb") as f:
+        save_data_loader_to_file(test_data_loader, f)
